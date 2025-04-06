7b554f083e408af9bd4641c2be602eba5d245fa4
Puszto <ifj.kosik.otto@gmail.com>
2025-04-06 12:08:33 +0200

    Teszt API controller létrehozása, IncomeData GET metódus implementálása

---
+++ BACKEND/Villanyszamla_backend/Controllers/ElectricityBillApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T0Y9UZ_FullStack_Feleves.Models;
+
+namespace T0Y9UZ_FullStack_Feleves.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ElectricityBillApiController : ControllerBase
+    {
+        public ElectricityBillApiController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<IncomeData> GetCalculation([FromQuery] IncomeData data)
+        {
+            
+            return new List<IncomeData>() { data };
+        }
+    }
+}
---
+++ BACKEND/Villanyszamla_backend/Models/IncomeData.cs
@@ -0,0 +1,8 @@
+﻿namespace T0Y9UZ_FullStack_Feleves.Models
+{
+    public class IncomeData
+    {
+        public string egysegar { get; set; }
+        public string datas { get; set; }
+    }
+}
