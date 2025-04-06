f225fe5262e5a693958a4fd2f18ff76452e5ad6f
Puszto <ifj.kosik.otto@gmail.com>
2025-04-06 03:32:45 +0200

    A kalkulátorhoz tartozó táblázat elkészült a válasz adatok megjelenítésére

--- FRONTEND/scripts/kalkulator.js
+++ FRONTEND/scripts/kalkulator.js
@@ -37,6 +37,11 @@ function FetchData(egysegar, datas) {
     })
     .then(responseData => {
         global = responseData[0];
+
+        TableDraw();
+        location.hash = '#';
+        location.hash = 'resultDiv';
+
     })
     .catch(error => {
         
@@ -112,4 +117,232 @@ function InputCheck(egysegar, datas) {
     }
 
     return helyes;
+}
+
+function TableDraw() {
+    let months = new Array();
+    months.push('Január');
+    months.push('Február');
+    months.push('Március');
+    months.push('Április');
+    months.push('Május');
+    months.push('Június');
+    months.push('Július');
+    months.push('Augusztus');
+    months.push('Szeptember');
+    months.push('Október');
+    months.push('November');
+    months.push('December');
+
+
+    let wrapper = document.getElementById('resultDiv');
+    RemoveResultDivElements(wrapper);
+
+    let wrappedDivForH1 = document.createElement('div');
+    wrappedDivForH1.id = 'calculationTitle';
+    wrappedDivForH1.style.width = 'fit-content';
+
+    let mainH1 = document.createElement('h1');
+    let now = new Date();
+    mainH1.classList.add('text-center');
+    mainH1.classList.add('text-warning');
+    mainH1.classList.add('font-monospace');
+    mainH1.innerHTML = now.getFullYear().toString().padStart(2, '0') + '. ' + now.getMonth().toString().padStart(2, '0') + '. ' + now.getDay().toString().padStart(2, '0') + '. ' + now.getHours().toString().padStart(2, '0') + ':' + now.getMinutes().toString().padStart(2, '0') + ':' + now.getSeconds().toString().padStart(2, '0') + ' - Számítás';
+
+    wrappedDivForH1.appendChild(mainH1);
+
+    let hr1 = document.createElement('hr');
+    hr1.classList.add('w-100');
+    hr1.classList.add('border');
+    hr1.classList.add('border-warning');
+    hr1.classList.add('border-2');
+    hr1.classList.add('opacity-100');
+    hr1.classList.add('rounded');
+    hr1.classList.add('rounded-5');
+    hr1.classList.add('mb-5');
+
+    wrappedDivForH1.appendChild(hr1);
+
+    wrapper.appendChild(wrappedDivForH1);
+
+    let evszam = global.evszam;
+    let haviKoltes = global.haviKoltes;
+
+    let table = document.createElement('table');
+    table.classList.add('table');
+    table.classList.add('table-dark');
+    table.classList.add('table-striped');
+    table.classList.add('table-hover');
+    table.classList.add('border');
+    table.classList.add('border-3');
+    table.classList.add('border-warning');
+    table.classList.add('m-0');
+    table.id = 'calculationTable';
+
+    let thead = document.createElement('thead');
+
+    let tr = document.createElement('tr');
+    let honapTh = document.createElement('th');
+    honapTh.innerHTML = '';
+    tr.appendChild(honapTh);
+
+    for (let i = 0; i < evszam.length; i++) {
+        const element = evszam[i];
+        const kedvezmenyesEvek = global.kedvezmenyesEvek[i];
+
+        let th = document.createElement('th');
+
+        if (kedvezmenyesEvek) {
+            th.innerHTML = element + '*';
+            th.classList.add('fs-2');
+            th.classList.add('text-info');
+            th.classList.add('text-center');
+            th.classList.add('align-middle');
+            th.classList.add('font-monospace');
+        }
+
+        else {
+            th.innerHTML = element;
+            th.classList.add('fs-2');
+            th.classList.add('text-warning');
+            th.classList.add('text-center');
+            th.classList.add('align-middle');
+            th.classList.add('font-monospace');
+        }
+
+        tr.appendChild(th);
+    }
+
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    let tbody = document.createElement('tbody');
+
+    for (let i = 0; i < months.length; i++) {
+        const element = months[i];
+
+        let tmp = global.haviKoltes[element];
+        let tmp2 = global.haviKoltes[element].length;
+
+        let sor = document.createElement('tr');
+        let tdHonap = document.createElement('td');
+        tdHonap.innerHTML = element;
+        tdHonap.classList.add('align-middle');
+        tdHonap.classList.add('text-warning');
+        tdHonap.classList.add('fs-5');
+        tdHonap.classList.add('fw-bold');
+        tdHonap.classList.add('font-monospace');
+        sor.appendChild(tdHonap);
+
+        for (let j = 0; j < tmp2; j++) {
+            const element = tmp[j];
+            const kedvezmenyesEvek = global.kedvezmenyesEvek[j];
+            let td = document.createElement('td');
+
+            if (kedvezmenyesEvek) {
+                td.innerHTML = element.toLocaleString('hu-HU') + '*' + ' HUF';
+                td.classList.add('text-center');
+                td.classList.add('text-info');
+                td.classList.add('align-middle');
+                td.classList.add('font-monospace');
+            }
+
+            else {
+                td.innerHTML = element.toLocaleString('hu-HU') + ' HUF';
+                td.classList.add('text-center');
+                td.classList.add('align-middle');
+                td.classList.add('font-monospace');
+            }
+            sor.appendChild(td);
+        }
+        tbody.appendChild(sor);
+    }
+
+    let EvesKoltsegSor = document.createElement('tr');
+    let EvesKoltsegTd = document.createElement('td');
+    let EvesKoltsegh5 = document.createElement('h5');
+    EvesKoltsegTd.classList.add('p-0');
+    EvesKoltsegTd.classList.add('m-0');
+
+    EvesKoltsegh5.innerHTML = 'Éves Költség';
+    EvesKoltsegh5.classList.add('bg-danger');
+    EvesKoltsegh5.classList.add('text-warning');
+    EvesKoltsegh5.classList.add('fw-bold');
+    EvesKoltsegh5.classList.add('py-3');
+    EvesKoltsegh5.classList.add('px-2');
+    EvesKoltsegh5.classList.add('m-0');
+    EvesKoltsegh5.classList.add('align-middle');
+    EvesKoltsegh5.classList.add('font-monospace');
+
+    EvesKoltsegTd.appendChild(EvesKoltsegh5);
+
+    EvesKoltsegSor.appendChild(EvesKoltsegTd);
+
+    let tmp3 = global.evesKoltseg;
+    let tmp4 = global.evesKoltseg.length;
+
+    for (let i = 0; i < tmp4; i++) {
+        const element = tmp3[i];
+        const kedvezmenyesEvek = global.kedvezmenyesEvek[i];
+
+        let td = document.createElement('td');
+        td.classList.add('p-0');
+        td.classList.add('m-0');
+
+        let Evesh5 = document.createElement('h5');
+        if (kedvezmenyesEvek) {
+            Evesh5.innerHTML = element.toLocaleString('hu-HU') + '*' +' HUF';
+            Evesh5.classList.add('text-info');
+        }
+        else {
+            Evesh5.innerHTML = element.toLocaleString('hu-HU') + ' HUF';
+        }
+        Evesh5.classList.add('bg-danger');
+        Evesh5.classList.add('fw-bold');
+        Evesh5.classList.add('m-0');
+        Evesh5.classList.add('p-3');
+        Evesh5.classList.add('h-100');
+        Evesh5.classList.add('text-center');
+        Evesh5.classList.add('align-middle');
+
+        td.appendChild(Evesh5);
+        EvesKoltsegSor.appendChild(td);
+    }
+
+    tbody.appendChild(EvesKoltsegSor);
+
+    table.appendChild(tbody);
+
+    wrapper.appendChild(table);
+
+    let randomButton = document.createElement('button');
+    randomButton.id = 'EditButton';
+    randomButton.classList.add('btn');
+    randomButton.classList.add('btn-danger');
+    randomButton.classList.add('border');
+    randomButton.classList.add('border-2');
+    randomButton.classList.add('border-warning');
+    randomButton.classList.add('text-warning');
+    randomButton.classList.add('w-75');
+    randomButton.classList.add('m-3');
+    randomButton.innerHTML = 'Szerkesztés'
+    randomButton.onclick = () => Edit();
+    wrapper.appendChild(randomButton);
+
+    let hr2 = document.createElement('hr');
+    hr2.id = 'calculationHr';
+    hr2.classList.add('w-100');
+    hr2.classList.add('border');
+    hr2.classList.add('border-warning');
+    hr2.classList.add('border-2');
+    hr2.classList.add('opacity-100');
+    hr2.classList.add('rounded');
+    hr2.classList.add('rounded-5');
+    hr2.classList.add('m-0');
+
+    wrapper.appendChild(hr2);
+
+
+    wrapper.classList.add('resultDivVisible');
+
 }
